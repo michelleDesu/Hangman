06b2535179d8bc67269cb73f6d0faa3aa0e9d5d5
michelle <michelle.johansson85@gmail.com>
2020-04-24 07:48:25 +0200

    init commit

---
+++ src/test/java/se/lexicon/michelle/michelle/AppTest.java
@@ -0,0 +1,22 @@
+package se.lexicon.michelle.michelle;
+
+
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
